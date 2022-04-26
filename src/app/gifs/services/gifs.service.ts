ed8701187b5e456e8c4c16c5244b2cb460d017a3
angeltonio <angeltonio86@gmail.com>
2022-04-26 17:00:00 -0400

    update

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -17,18 +17,30 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+ 
+  }
+
+
  
   buscarGifs(query: string) {
      query= query.trim().toLowerCase();
     if(!this._historial.includes(query)){
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
-      
+    
+    
+
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=T5GdUX32bFQ1PbDxGCfdCSxVybG3O7J8&q=${query}&limit=10`)
     .subscribe((resp)=>{
     this.resultados=resp.data;
+    localStorage.setItem('resultados', JSON.stringify(this.resultados));
+
   
   })
     
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -8,9 +8,14 @@ import { GifsService } from '../../gifs/services/gifs.service';
   ]
 })
 export class SidebarComponent  {
-
+  
   constructor(private gifsService: GifsService) { }
-   
+  
+  
+  buscar(termino:string) {
+    this.gifsService.buscarGifs(termino);
+  }
+
   get historial () {
     return this.gifsService.historial;
     
